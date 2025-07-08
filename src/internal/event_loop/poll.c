fc9f27992770df5de5375dab8189300213592f70
Guest0x0 <guest0x0@outlook.com>
2025-07-08 06:55:55 +0000

    experimental BSD/MacOS support via kqueue

--- src/internal/event_loop/poll.c
+++ src/internal/event_loop/poll.c
@@ -1,6 +1,7 @@
-#include <sys/epoll.h>
 #include <unistd.h>
-#include <stdio.h>
+
+#ifdef __linux__
+#include <sys/epoll.h>
 
 int poll_create() {
   return epoll_create1(0);
@@ -86,3 +87,80 @@ int event_get_events(struct epoll_event *ev) {
 
   return result;
 }
+
+// end of epoll backend
+#elif defined(__MACH__) || defined(BSD)
+
+#include <time.h>
+#include <sys/event.h>
+
+int poll_create() {
+  return kqueue();
+}
+
+void poll_destroy(int kqfd) {
+  close(kqfd);
+}
+
+const int ev_masks[] = {
+  0,
+  EVFILT_READ,
+  EVFILT_WRITE,
+  EVFILT_READ | EVFILT_WRITE,
+  EV_ERROR
+};
+
+int poll_register(int kqfd, int fd, int events, int oneshot) {
+  int filter = ev_masks[events];
+
+  int flags = EV_ADD | EV_CLEAR;
+  if (oneshot)
+    flags |= EV_DISPATCH;
+
+  struct kevent event;
+  EV_SET(&event, fd, filter, flags, 0, 0, 0);
+  return kevent(kqfd, &event, 1, 0, 0, 0);
+}
+
+int poll_modify(int kqfd, int fd, int events, int oneshot) {
+  return poll_register(kdfd, fd, events, oneshot);
+}
+
+int poll_remove(int kqfd, int fd) {
+  struct kevent event;
+  EV_SET(&event, fd, 0, EV_DELETE, 0, 0, 0);
+  return kevent(kqfd, &event, 1, 0, 0, 0);
+}
+
+#define EVENT_BUFFER_SIZE 1024
+struct kevent event_buffer[EVENT_BUFFER_SIZE];
+
+int poll_wait(int kqfd, int timeout) {
+  struct timespec timeout_spec = { timeout / 1000, (timeout / 1000) * 1000 };
+  return kevent(kqfd, 0, 0, &event_buffer, EVENT_BUFFER_SIZE, &timeout_spec);
+}
+
+// wrapper for handling event list
+struct kevent *event_list_get(int index) {
+  return event_buffer + index;
+}
+
+int event_get_fd(struct kevent *ev) {
+  return ev->ident;
+}
+
+int event_get_events(struct kevent *ev) {
+  if (ev->flags | EV_ERROR)
+    return 4;
+
+  int result = 0;
+  if (ev->filter & EVFILT_READ)
+    result |= 1;
+  if (ev->filter & EVFILT_WRITE)
+    result |= 2;
+
+  return result;
+}
+
+// end of kqueue backend
+#endif
