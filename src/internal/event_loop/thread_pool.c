cc2f989bf4d2aa714d51acfbdd6afdfbb4717131
Guest0x0 <guest0x0@outlook.com>
2025-08-15 05:55:01 +0000

    fix log

--- src/internal/event_loop/thread_pool.c
+++ src/internal/event_loop/thread_pool.c
@@ -175,7 +175,7 @@ void *worker(void *data) {
   struct job *job = *((struct job**)data);
 
   while (job) {
-    printf("worker %ul: received %d\n", self, job->job_id);
+    printf("worker %ld: received %d\n", self, job->job_id);
     job->ret = 0;
     job->err = 0;
 
@@ -359,7 +359,7 @@ void *worker(void *data) {
       break;
     }
     }
-    printf("worker %ul: %d completed\n", self, job->job_id);
+    printf("worker %ld: %d completed\n", self, job->job_id);
     write(pool.notify_send, &job, sizeof(struct job*));
 
     job = 0;
