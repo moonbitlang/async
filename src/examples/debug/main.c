72011ee2fc7e42ca9dfafade9511a226cdc73a3e
Guest0x0 <guest0x0@outlook.com>
2025-08-18 02:26:59 +0000

    simplify

--- src/examples/debug/main.c
+++ src/examples/debug/main.c
@@ -3,8 +3,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <sys/select.h>
 
 int ipc[2];
 
@@ -40,7 +38,7 @@ void *worker2(void *input) {
   }
 }
 
-void main_prog() {
+int main() {
   // initialize signal set
   sigset_t set;
   sigemptyset(&set);
@@ -50,12 +48,6 @@ void main_prog() {
 
   // initialize the pipe
   pipe(ipc);
-  int flags = fcntl(ipc[0], F_GETFL);
-  fcntl(ipc[0], F_SETFL, flags | O_NONBLOCK);
-
-  fd_set poll_set;
-  FD_ZERO(&poll_set);
-  FD_SET(ipc[0], &poll_set);
 
   // create two threads
   pthread_attr_t attr;
@@ -65,32 +57,16 @@ void main_prog() {
   pthread_create(&id1, &attr, worker1, &set);
   pthread_create(&id2, &attr, worker2, &set);
 
-  for (int i = 0; i < 1000; ++i) {
-    int worker1_done = 0;
-    int worker2_done = 0;
-    int data = 0;
+  for (int i = 0; i < 10000; ++i) {
+    int data[2];
 
-    select(ipc[0] + 1, &poll_set, 0, 0, 0);
-    while (read(ipc[0], &data, sizeof(int)) > 0) {
-      if (data == 1)
-        worker1_done = 1;
-      else if (data == 2)
-        worker2_done = 1;
-      else {
-        printf("impossible\n");
-        abort();
-      }
-    }
+    read(ipc[0], &data, 2 * sizeof(int));
 
-    if (worker1_done) {
-      // printf("sending signal %d to worker 1\n", SIGUSR1);
-      pthread_kill(id1, SIGUSR1);
-    }
+    // printf("sending signal %d to worker 1\n", SIGUSR1);
+    pthread_kill(id1, SIGUSR1);
 
-    if (worker2_done) {
-      // printf("sending signal %d to worker 2\n", SIGUSR2);
-      pthread_kill(id2, SIGUSR2);
-    }
+    // printf("sending signal %d to worker 2\n", SIGUSR2);
+    pthread_kill(id2, SIGUSR2);
   }
 
   exit(0);
