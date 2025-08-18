aa91b5fc0cf3d35dfbae759260323b51dcae7641
Guest0x0 <guest0x0@outlook.com>
2025-08-18 05:45:28 +0000

    simplify

--- src/examples/debug/main.c
+++ src/examples/debug/main.c
@@ -4,35 +4,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int ipc[2];
+int channel[2];
+sigset_t set;
 
-void *worker1(void *input) {
-  sigset_t *set = (sigset_t*)input;
+void *worker(void *input) {
   int sig;
-  int data = 1;
+  int expected_sig = *(int*)input;
   for (int i = 0; ; ++i) {
-    write(ipc[1], &data, sizeof(int));
-    // printf("worker 1 start waiting\n");
-    sigwait(set, &sig);
-    // printf("worker 1 received signal %d\n", sig);
-    if (sig != SIGUSR1) {
-      printf("worker 1 received incorrect signal at iteration %d\n", i);
-      abort();
-    }
-  }
-}
-
-void *worker2(void *input) {
-  sigset_t *set = (sigset_t*)input;
-  int sig;
-  int data = 2;
-  for (int i = 0; ; ++i) {
-    write(ipc[1], &data, sizeof(int));
-    // printf("worker 2 start waiting\n");
-    sigwait(set, &sig);
-    // printf("worker 2 received signal %d\n", sig);
-    if (sig != SIGUSR2) {
-      printf("worker 2 received incorrect signal at iteration %d\n", i);
+    // notify the main thread that we are done
+    write(channel[1], &expected_sig, sizeof(int));
+    // wait for signal from the main thread
+    sigwait(&set, &sig);
+    if (sig != expected_sig) {
+      printf("received incorrect signal at iteration %d\n", i);
       abort();
     }
   }
@@ -40,33 +24,33 @@ void *worker2(void *input) {
 
 void main_prog() {
   // initialize signal set
-  sigset_t set;
   sigemptyset(&set);
   sigaddset(&set, SIGUSR1);
   sigaddset(&set, SIGUSR2);
   pthread_sigmask(SIG_BLOCK, &set, 0);
 
   // initialize the pipe
-  pipe(ipc);
+  pipe(channel);
 
   // create two threads
   pthread_attr_t attr;
   pthread_attr_init(&attr);
 
   pthread_t id1, id2;
-  pthread_create(&id1, &attr, worker1, &set);
-  pthread_create(&id2, &attr, worker2, &set);
+  int thread1_expected_sig = SIGUSR1;
+  int thread2_expected_sig = SIGUSR2;
+  pthread_create(&id1, &attr, worker, &thread1_expected_sig);
+  pthread_create(&id2, &attr, worker, &thread2_expected_sig);
 
   for (int i = 0; i < 10000; ++i) {
     int data[2];
-
-    read(ipc[0], &data, 2 * sizeof(int));
-
-    // printf("sending signal %d to worker 1\n", SIGUSR1);
-    pthread_kill(id1, SIGUSR1);
-
-    // printf("sending signal %d to worker 2\n", SIGUSR2);
-    pthread_kill(id2, SIGUSR2);
+    // wait for the two threads to finish processing.
+    // Note that this is only for avoiding duplicated signal.
+    // It does not matter if `pthread_kill` is executed before or after `sigwait`,
+    // as the signal is blocked.
+    read(channel[0], &data, 2 * sizeof(int));
+    pthread_kill(id1, thread1_expected_sig);
+    pthread_kill(id2, thread2_expected_sig);
   }
 
   exit(0);
