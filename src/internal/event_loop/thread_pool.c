817c8d93cfc6a85ba44054bcef83cb23b19eb25e
Guest0x0 <guest0x0@outlook.com>
2025-08-31 21:43:19 +0800

    fix

--- src/internal/event_loop/thread_pool.c
+++ src/internal/event_loop/thread_pool.c
@@ -406,8 +406,9 @@ void *worker_loop(void *data) {
 void moonbitlang_async_wake_worker(struct worker *worker, struct job *job) {
   moonbit_decref(worker->job);
   worker->job = job;
-  if (job->op_code == OP_READDIR) {
+  if (job && job->op_code == OP_READDIR) {
     printf("before submitting readdir job\n");
+    fflush(0);
   }
 #ifdef WAKEUP_METHOD_SIGNAL
   pthread_kill(worker->id, SIGUSR1);
@@ -416,8 +417,9 @@ void moonbitlang_async_wake_worker(struct worker *worker, struct job *job) {
   pthread_cond_signal(&(worker->cond));
   pthread_mutex_unlock(&(worker->mutex));
 #endif
-  if (job->op_code == OP_READDIR) {
+  if (job && job->op_code == OP_READDIR) {
     printf("after submitting readdir job\n");
+    fflush(0);
   }
 }
 
