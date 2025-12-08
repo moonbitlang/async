3fe83c5777adb640edbc536fb2f801e4089d6201
Guest0x0 <guest0x0@outlook.com>
2025-12-08 15:46:55 +0800

    port `moonbitlang/async/internal/c_buffer`

--- src/internal/c_buffer/stub.c
+++ src/internal/c_buffer/stub.c
@@ -17,23 +17,29 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <moonbit.h>
 
+MOONBIT_FFI_EXPORT
 void moonbitlang_async_blit_to_c(char *dst, char *src, int offset, int len) {
   memcpy(dst, src + offset, len);
 }
 
+MOONBIT_FFI_EXPORT
 void moonbitlang_async_blit_from_c(char *src, char *dst, int offset, int len) {
   memcpy(dst + offset, src, len);
 }
 
+MOONBIT_FFI_EXPORT
 int moonbitlang_async_c_buffer_get(uint8_t *buf, int index) {
   return buf[index];
 }
 
+MOONBIT_FFI_EXPORT
 int32_t moonbitlang_async_strlen(char *str) {
   return strlen(str);
 }
 
+MOONBIT_FFI_EXPORT
 char *moonbitlang_async_null_pointer() {
   return 0;
 }
