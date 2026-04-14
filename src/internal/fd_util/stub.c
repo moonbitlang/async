75d8d308904e605371e8f394362cc9aecbcf2ad6
Guest0x0 <guest0x0@outlook.com>
2026-04-14 11:07:57 +0800

    make the abstract `HANDLE` type for Windows publicly available

--- src/internal/fd_util/stub.c
+++ src/internal/fd_util/stub.c
@@ -32,10 +32,6 @@
 
 
 #ifdef _WIN32
-MOONBIT_FFI_EXPORT
-uint64_t moonbitlang_async_fd_as_uint64(HANDLE handle) {
-  return (uint64_t)handle;
-}
 
 MOONBIT_FFI_EXPORT
 int32_t moonbitlang_async_fd_is_valid(HANDLE handle) {
